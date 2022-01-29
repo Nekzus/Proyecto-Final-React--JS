fe5591e52b6816dbb27b65a9b904b8ea243befe9
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-01-29 12:24:15 -0300

    Enrutamiento correcto url logo

--- src/components/Common/LogoPage.js
+++ src/components/Common/LogoPage.js
@@ -3,7 +3,7 @@ import Image from "react-bootstrap/Image";
 
 const LogoPage = () => {
     return (
-        <Image id="logo-page" src="https://firebasestorage.googleapis.com/v0/b/dbcinenkmax-17010.appspot.com/o/Logo%20Cines%20NKMAX%2FLogoNkMaxShadow.png?alt=media&token=eacb85ff-b252-430f-ba71-d876cfcdf255" fluid />
+        <Image id="logo-page" src="https://firebasestorage.googleapis.com/v0/b/dbcinenkmax-17010.appspot.com/o/Logo%20Cines%20NKMAX%2FLogoNkMaxShadow.png?alt=media&token=2b47d6a9-7dc8-4795-867f-d5ee49c34873" fluid />
     )
 };
 export default LogoPage;
